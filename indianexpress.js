a07cc2517d2572b18a87920765be649cddf7576f
united-geekdom <kirannithilan@gmail.com>
2023-12-20 18:25:16 +0530

    Add Indian Express

--- indianexpress.js
+++ indianexpress.js
@@ -1,3 +1,7 @@
-document.getElementsByClassName("ie-premium-content-block")[0].style.display = "inline-block";
-document.getElementsByClassName("content-login-wrapper")[0].style.display = "none";
-document.getElementsByClassName("subscriber_hide")[0].style.display = "none"; 
+if(document.title.includes("The Indian Express")) {
+    document.getElementsByClassName("ie-premium-content-block")[0].style.display = "inline-block";
+    document.getElementsByClassName("content-login-wrapper")[0].style.display = "none";
+    document.getElementsByClassName("subscriber_hide")[0].style.display = "none"; 
+} else {
+    alert("Please use this on an Indian Express article.")
+}

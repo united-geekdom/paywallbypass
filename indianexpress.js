92bfe2b7ef93cdf26bca0d3da68f5daf97ddfa00
united-geekdom <kirannithilan@gmail.com>
2023-12-20 18:16:51 +0530

    add indianexpress

---
+++ indianexpress.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("ie-premium-content-block")[0].style.display = "inline-block";
+document.getElementsByClassName("content-login-wrapper")[0].style.display = "none";
+document.getElementsByClassName("subscriber_hide")[0].style.display = "none"; 
